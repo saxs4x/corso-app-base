005ff6d79f8be4de2f8254d15bcead4fe90aaa73
voidBit <saccullo.francesco@gmail.com>
2022-09-29 18:49:36 +0200

    Ho creato componente base CounterScreen

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const CounterScreen = () => {
+    return <Text>Counter</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
