71b68abd316a9a44d455e4d31ffe2ff66483961f
voidBit <saccullo.francesco@gmail.com>
2022-10-06 19:07:20 +0200

    Color Screen modified+

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { 
     View, 
     Text, 
     StyleSheet, 
-    TouchableOpacity } 
+    TouchableOpacity,
+    FlatList, 
+    useColorScheme} 
 from 'react-native';
 
 const ColorScreen = () => {
+    const [colors, setColors] = useState([]);
+    console.log(colors)
    return (
     <View>
-        <TouchableOpacity>
-            <Text>Aggiungi Rosso</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-            <Text>Aggiungi Verde</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-            <Text>Aggiungi Blu</Text>
-        </TouchableOpacity>
-        <View style={{
-            backgroundColor: `rgb(0, 0, 0)`, 
-            width: 100, height: 100                  
+        <TouchableOpacity style={styles.button} onPress={() => {
+            setColors([...colors, randomColor()])
         }}>
-        </View>
+            <Text>Genera Colore</Text>
+        </TouchableOpacity>
+        <FlatList 
+            data={colors}
+            keyExtractor={(item) => item}
+            renderItem={({item}) => {
+                return <View style={styles.colorBox}>
+                    <Text>{item}</Text>
+                    </View>
+            }}
+        />
     </View>
    )
 }
 
-const styles = StyleSheet.create({});
+const randomColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+} 
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 10,
+        borderWidth: 1,
+        alignItems: 'center'
+    },
+    colorBox: {
+        width: 100,
+        height: 100
+    }
+});
 
 export default ColorScreen;
